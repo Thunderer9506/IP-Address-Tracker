b82605210ef934868481ac3a0b8ccad7f57d31b8
Shaurya Srivastava <srivastavashaurya225@gmail.com>
2024-05-02 22:35:24 +0530

    completed the Project

---
+++ main.js
@@ -0,0 +1,43 @@
+
+document.querySelector(".searchButton button").addEventListener("click",function(){
+  let ip = document.querySelector(".searchButton input").value
+  let apiUrl = "https://geo.ipify.org/api/v2/country,city?apiKey=at_gQIXcNKidkzQ3NRoHnDmmEhGQZPVU&ipAddress=" + ip
+  getdata(apiUrl)
+})
+
+function getMap(lat,lng){
+  let map = L.map('map').setView([lat, lng], 15);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map)
+  let marker = L.marker([lat, lng]).addTo(map)
+}
+
+function info(ip,loc,time,isp){
+  document.querySelector(".ip h1").innerText = ip
+  document.querySelector(".location h1").innerText = loc
+  document.querySelector(".timezone h1").innerText = time
+  document.querySelector(".isp h1").innerText = isp
+}
+
+// Make a GET request
+function getdata(url){
+  fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    info(data.ip,
+      data.location.city +","+ data.location.country+","+ data.location.postalCode,
+      data.location.timezone,
+      data.isp)
+    getMap(data.location.lat,data.location.lng)
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
